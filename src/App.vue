821a661e6b6e3cfddd4252601b9e1328fc08d106
Albermonte <alberto.monterroso@hotmail.com>
2018-12-07 19:26:14 +0100

    Tweet feed

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+html, body {
+  height: 100%;
+  padding-top: 10px;
+  background: #e6ecf1;
+}
+
+#app {
+  height: 100%;
+  padding-top: 0px;
+  text-align: center;
+}
+
+#app .button {
+  margin-bottom: 20px;
+}
+
+#app .box {
+  margin-bottom: 0;
+  border-radius: 15;
+}
+
+#app .content small {
+  color: #00d1b2;
+}
+
+#app img {
+  border-radius: 30px;
+}
+
+#app .level-item {
+  padding-left: 10px;
+  color: #00d1b2;
+}
+
+#app input:focus {
+  border-color: #00d1b2;  
+}
+
+#app .likes {
+  padding: 0 7.5px;
+}
+
+#app input {
+  font-weight: bold;
+}
+
+#app .tweets-move {
+  transition: transform 1s;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="columns">
+    <div class="column">
+      <tweetComponent v-for="tweet in tweets" :tweet="tweet"/>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import tweetComponent from "@/components/tweetComponent.vue";
+
+export default {
+  name: "home",
+  components: {
+    tweetComponent
+  },
+  data(){
+    return {
+    tweets: [
+      {
+        id: 1,
+        name: 'James',
+        handle: '@jokerjames',
+        img: 'https://semantic-ui.com/images/avatar2/large/matthew.png',
+        tweet: "If you don't succeed, dust yourself off and try again.",
+        likes: 10,
+      },
+      { 
+        id: 2,
+        name: 'Fatima',
+        handle: '@fantasticfatima',
+        img: 'https://semantic-ui.com/images/avatar2/large/molly.png',
+        tweet: 'Better late than never but never late is better.',
+        likes: 12,
+      },
+      {
+        id: 3,
+        name: 'Xin',
+        handle: '@xeroxin',
+        img: 'https://semantic-ui.com/images/avatar2/large/elyse.png',
+        tweet: 'Beauty in the struggle, ugliness in the success.',
+        likes: 18,
+      }
+    ]
+    }
+  }
+};
+</script>
